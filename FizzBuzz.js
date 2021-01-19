d7081094b0c379f92e864db8c9334584695588c7
kotita49 <kotita49@gmail.com>
2021-01-19 08:56:40 +0000

    Added FizzBuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,8 +1,12 @@
 function fizzBuzz(){
     for(i=1; i<=100; i++){
-        if(i%3==0){
+        if(i%3==0&&i%5==0){
+            console.log("FizzBuzz")
+        }else if(i%3==0){
             console.log("Fizz")
-        } else{
+        } else if(i%5==0){
+            console.log("Buzz")
+        }        else{
     
     console.log(i)
 }
