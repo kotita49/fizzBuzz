7fff9bfd5e1ffee3120141fdce2c7a5e849a1933
kotita49 <kotita49@gmail.com>
2021-01-19 10:34:45 +0000

    Added Bang

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,12 +1,21 @@
 function fizzBuzz(){
     for(i=1; i<=100; i++){
-        if(i%3==0&&i%5==0){
+        if(i%3==0&&i%5==0&&i%7==0){
+            console.log("FizzBuzzBang")
+        }else if(i%3==0&&i%7==0){
+            console.log("FizzBang")
+        } else if(i%5==0&&i%7==0){
+            console.log("BuzzBang")
+        }else if(i%3==0&&i%5==0){
             console.log("FizzBuzz")
+        } else if(i%7==0){
+            console.log("Bang")
         }else if(i%3==0){
             console.log("Fizz")
-        } else if(i%5==0){
+        }else if(i%5==0){
             console.log("Buzz")
-        }        else{
+        }
+        else{
     
     console.log(i)
 }
