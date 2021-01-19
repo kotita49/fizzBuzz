52bb9409457119952fe5c67c12d056a87e71b78c
kotita49 <kotita49@gmail.com>
2021-01-19 10:38:12 +0000

    Bong

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,6 +1,8 @@
 function fizzBuzz(){
     for(i=1; i<=100; i++){
-        if(i%3==0&&i%5==0&&i%7==0){
+        if(i%11==0){
+            console.log("Bong")
+        }else if(i%3==0&&i%5==0&&i%7==0){
             console.log("FizzBuzzBang")
         }else if(i%3==0&&i%7==0){
             console.log("FizzBang")
